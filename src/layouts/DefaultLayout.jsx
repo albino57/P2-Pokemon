87cca72e8b0eba4fd9f12be2c2a1a431aeb46bb2
paulocesar-neto <paulo.valeneto@gmail.com>
2026-06-14 20:25:56 -0300

    estilizando layout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,19 +1,25 @@
+import { useState, useEffect, useContext } from "react";
 import { Outlet } from "react-router";
+import style from './styles.module.css';
 
 export const DefaultLayout = () => {
   return (
-    <div>
-      <header>
-        <h1>Cabeçalho temporario</h1>
-      </header>
-
-      <main>
-        <Outlet /> 
-      </main>
-
-      <footer>
-        <p>Rodapé Temporario</p>
-      </footer>
+    <div className={style.layoutContainer}>
+      <div className={style.carousel}>
+        koee
+      </div>
+      <div className={style.mainContainer}>
+        <div className={style.lateralBar}>
+         </div>
+        <main className= {style.main}>
+          <div className={style.divOutlet}>
+            <Outlet />
+          </div>
+          <footer>
+            <p>Rodapé Temporario</p>
+          </footer>
+        </main>
+      </div>
     </div>
   );
 };
