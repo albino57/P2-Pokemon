e5a71f455c18100ede5874b9817de5a2bfa2651c
paulocesar-neto <paulo.valeneto@gmail.com>
2026-06-15 17:48:00 -0300

    estilizando home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,15 @@ import styles from "./Home.module.css";
 
 export const Home = () => {
 
-
-    
     // <div className={styles.container}> ... </div>
-    return <h2>Home Page - Pokemon List</h2>;
-
-
+    return <>
+    
+       <h2>Pokédex - Pokémon</h2>
+        <h4>Pokédex - Pokémon é um projeto Rect de cards colecionáveis de Pokémons,
+        onde você pode capturar e armazenar seus Pokémons favoritos na sua Pokédex, e solta-los a hora que quiser
+        busque seus Pokémons na aba de pesquisa ou capture-os na aba Pokémons, administre seus cards na aba Pokédex 
+        onde são armazenados os Pokémons capturados, viva a aventura e caputere seus Pokémons favoritos</h4>
+      
+    </>
 
 }
