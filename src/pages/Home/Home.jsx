ac240855465d66fbca2c07c4ccb9f94d9ccffa43
paulocesar-neto <paulo.valeneto@gmail.com>
2026-06-15 21:48:07 -0300

    montando página home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,13 +5,21 @@ export const Home = () => {
 
     // <div className={styles.container}> ... </div>
     return <>
-    
-       <h2>Pokédex - Pokémon</h2>
-        <h4>Pokédex - Pokémon é um projeto Rect de cards colecionáveis de Pokémons,
-        onde você pode capturar e armazenar seus Pokémons favoritos na sua Pokédex, e solta-los a hora que quiser
-        busque seus Pokémons na aba de pesquisa ou capture-os na aba Pokémons, administre seus cards na aba Pokédex 
-        onde são armazenados os Pokémons capturados, viva a aventura e caputere seus Pokémons favoritos</h4>
-      
+        <div className={styles.sobreContainer}>
+            <img className={styles.pokemons} src="src\assets\pokemons.png" alt="" />
+            <div className={styles.txtBox}>
+                <h1 className={styles.titlePokedex}>Pokédex - Pokémon</h1>
+                <p className={styles.paragraph}>Pokédex - Pokémon é um projeto React de cards colecionáveis de Pokémons,
+                    onde você pode capturar e armazenar seus Pokémons favoritos na sua Pokédex, e solta-los a hora que quiser.
+                    Busque seus Pokémons na aba de pesquisa ou capture-os na aba Pokémons, administre seus cards na aba Pokédex
+                    onde são armazenados os Pokémons capturados, viva a aventura e capture  Pokémons e se torne um treinador
+                    de Pokémons</p>
+
+                    
+            </div>
+
+        </div>
+
     </>
 
 }
