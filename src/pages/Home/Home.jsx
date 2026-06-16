5fa1578046b2b89c9a9bc472341bed08c503df48
paulocesar-neto <paulo.valeneto@gmail.com>
2026-06-15 23:19:17 -0300

    constrindo estilização page home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,9 @@ import styles from "./Home.module.css";
 export const Home = () => {
 
     // <div className={styles.container}> ... </div>
-    return <>
+    return (
+    
+    <div className={styles.container} >
         <div className={styles.sobreContainer}>
             <img className={styles.pokemons} src="src\assets\pokemons.png" alt="" />
             <div className={styles.txtBox}>
@@ -14,12 +16,17 @@ export const Home = () => {
                     Busque seus Pokémons na aba de pesquisa ou capture-os na aba Pokémons, administre seus cards na aba Pokédex
                     onde são armazenados os Pokémons capturados, viva a aventura e capture  Pokémons e se torne um treinador
                     de Pokémons</p>
-
-                    
             </div>
-
+        </div>
+            <div className={styles.sobreContainer2}>
+            <div className={styles.txtBox2}>
+                <h1 className={styles.titlePokedex2}>Explore Tipos de Pokémons</h1>
+                <p className={styles.paragraph2}>Busque e capture seus Pókemons pelo tipo, 
+                    monte seu deck de cards Pokémons escolhendo pelo tipo e suas fraquezas</p>
+            </div>
+            <img className={styles.pokemonsTipos} src="src\assets\tipoIcon.png" alt="" />
         </div>
 
-    </>
+    </div>)
 
 }
