eefb6e8ac9a3b96f071beeb8e0ec9646b06722c0
paulocesar-neto <paulo.valeneto@gmail.com>
2026-06-16 02:14:58 -0300

    quase conclusao estilização home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -39,15 +39,12 @@ export const Home = () => {
 
             </div>
             <div className={styles.sobreContainer}>
-                <img className={styles.pokemons} src="src\assets\pokemons.png" alt="" />
+
                 <div className={styles.txtBox}>
-                    <h1 className={styles.titlePokedex}>Pokédex - Pokémon</h1>
-                    <p className={styles.paragraph}>Pokédex - Pokémon é um projeto React de cards colecionáveis de Pokémons,
-                        onde você pode capturar e armazenar seus Pokémons favoritos na sua Pokédex, e solta-los a hora que quiser.
-                        Busque seus Pokémons na aba de pesquisa ou capture-os na aba Pokémons, administre seus cards na aba Pokédex
-                        onde são armazenados os Pokémons capturados, viva a aventura e capture  Pokémons e se torne um treinador
-                        de Pokémons</p>
+                    <h1 className={styles.titlePokedex}>Se divirta</h1>
+            
                 </div>
+                <img className={styles.poke} src="src\assets\pokes.png" alt="" />
             </div>
 
 
