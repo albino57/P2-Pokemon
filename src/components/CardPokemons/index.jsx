5d7624c7e05b084ed7452d72d5e6887ee01b549b
marcospmelloo <marcospoliveiramello@gmail.com>
2026-06-17 00:54:53 -0300

    feat: adiciona loading para capturar pokemons

--- src/components/CardPokemons/index.jsx
+++ src/components/CardPokemons/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PokedexContext } from "../../contexts/PokedexContext";
 import style from './styles.module.css'
 
@@ -10,16 +10,36 @@ export const CardPokemons = ({ pokemon }) => {
    const imagemPokemon = pokemon.sprites?.other['official-artwork']?.front_default 
                           || pokemon.sprites?.front_default;
 
+    const [status, setStatus] = useState('idle');
+    
+    function handleCapturar() {
+        setStatus('capturando');
+
+        setTimeout(() => {
+            addPokemon(pokemon);
+            setStatus('capturado');
+        }, 1500);
+
+        setTimeout(() => {
+            setStatus('idle');
+        }, 3000);
+    }          
+    
+    function handleSoltar() {
+        dropPokemon(pokemon.id);
+    }
+
     return (
         <div className={style.card}>
             <img src={imagemPokemon} alt={pokemon.name} className={style.pokeImage} />
             <h3>{pokemon.name}</h3>
-            <button onClick={() => isCaptured ? dropPokemon(pokemon.id) : addPokemon(pokemon)}>
+            {status == 'idle' && (
+            <button onClick={() => isCaptured ? handleSoltar() : handleCapturar()}>
                 {isCaptured ? "Soltar" : "Capturar"}
             </button>
+            )}
+            {status === 'capturando' && <p>Capturando...</p>}
+            {status === 'capturado' && <p>Capturado!</p>}
         </div>
     )
-
-
-
 }
