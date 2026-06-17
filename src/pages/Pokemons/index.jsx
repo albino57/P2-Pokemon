1c01053ba81e1c0cba5d823cab112d90f55d3465
paulocesar-neto <paulo.valeneto@gmail.com>
2026-06-17 03:09:56 -0300

    feito um pouco da estilização dos cards

--- src/pages/Pokemons/index.jsx
+++ src/pages/Pokemons/index.jsx
@@ -5,10 +5,10 @@ import styles from './styles.module.css'
 export const Pokemons = () => {
 
     return (
-        <div className={styles.container}>
-         <img className = {styles.logoPoke} src="src/assets/pikachu.png" alt="" />
+        
+         
            <PokemonList/>
-        </div>
+        
          
     )
 }
