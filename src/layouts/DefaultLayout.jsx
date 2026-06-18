e74baae456a22ddce29f5c8307cf95340105d50f
brenofranca2000 <brenofranca2000@gmail.com>
2026-06-17 21:38:25 -0300

    Feat: instalei react-icons, apliquei íccones nos botoes e na navbar, adicionei biblioteca de alerta toastify

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,24 +2,28 @@ import { useState, useEffect, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import style from './styles.module.css';
 import { Searchbar } from "../components/Searchbar";
-
+import { FaHome, FaGamepad, FaPlusCircle } from 'react-icons/fa';
 
 export const DefaultLayout = () => {
-
   return (
     <div className={style.layoutContainer}>
       <nav className={style.carousel}>
-        <img className={style.navLogo} src="src/assets/logoPokemon.png" alt="pokedex" />
-      </nav>
-      <nav className={style.subNav}>
-        <div className={style.linkNavContainer}>
-          <Link className={style.linkNav}  to="/home">Home</Link>
-          <Link className={style.linkNav}  to="/pokemon">Pokémons</Link>
-          <Link className={style.linkNav}  to="/pokedex">Pokédex</Link>
-          <Link className={style.linkNav} to = "/creator">New Pokémons</Link>
-           <Searchbar 
-              />
-        </div>
+        <img className={style.navLogo} src="src/assets/logoPokemon.png" alt="pokemon" />
+        <nav className={style.subNav}>
+          <div className={style.linkNavContainer}>
+            <Link className={style.linkNav} to="/home">
+              <FaHome style={{ marginRight: '6px' }} /> Home
+            </Link>
+            <Link className={style.linkNav} to="/pokemon">Pokémons</Link>
+            <Link className={style.linkNav} to="/pokedex">
+              <FaGamepad style={{ marginRight: '6px' }} /> Pokédex
+            </Link>
+            <Link className={style.linkNav} to="/creator">
+              <FaPlusCircle style={{ marginRight: '6px' }} /> New Pokémons
+            </Link>
+            <Searchbar />
+          </div>
+        </nav>
       </nav>
 
       <div className={style.mainContainer}>
@@ -27,16 +31,8 @@ export const DefaultLayout = () => {
           <div className={style.divOutlet}>
             <Outlet />
           </div>
-          
         </main>
-        
       </div>
-      <footer>
-            <p>Pokemon © 2026 - Projeto Serratec</p>
-            <Link to="/contato" className="footerLink">
-              Contato
-            </Link>
-          </footer>
     </div>
   );
 };
