8fcbace8c38e0224ba26168baaee001b7bb45acd
paulocesar-neto <paulo.valeneto@gmail.com>
2026-06-22 19:30:21 -0300

    mudando icone da pagina

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -1,50 +1,67 @@
-import { useState,useContext } from "react";
-import style from './styles.module.css';
-import { getPokemonDetails, searchPokemon } from "../../services/PokeAPI/pokeAPI";
+import { useState } from "react";
+import style from "./styles.module.css";
+import { getPokemonDetails } from "../../services/PokeAPI/pokeAPI";
 import { usePokedex } from "../../contexts/PokedexContext";
-import { CardPokemons } from "../CardPokemons";
-
 
 export const Searchbar = () => {
-    const [search, setSearch] = useState("")
-    const [pokemon, setPokemon] = useState();
+    const [search, setSearch] = useState("");
 
-    
-    const{setPokeSearch} = usePokedex();
+    const { setPokeSearch } = usePokedex();
 
     const onChangeHandle = (e) => {
         const value = e.target.value;
+
         setSearch(value);
 
-        if(value === ""){
-            setPokeSearch(null)
+        if (value === "") {
+            setPokeSearch(null);
         }
-    }
+    };
 
     const onButtonClickHandle = (e) => {
         e.preventDefault();
-        if(search.trim() !==""){
 
+        if (search.trim() !== "") {
             onSearchHandle(search.toLowerCase().trim());
         }
-    }
+    };
 
     const onSearchHandle = async (pokemon) => {
-        const results = await getPokemonDetails(pokemon)
-        setPokeSearch(results.data)
-    }
+        try {
+            const results = await getPokemonDetails(pokemon);
+
+            setPokeSearch(results.data);
+
+        } catch (err) {
+
+            setPokeSearch(null);
+
+            alert(`O Pokémon "${pokemon}" não foi encontrado na Pokédex.`);
+        }
+    };
 
     return (
-
         <div>
             <div className={style.searchBar}>
-                <input className={style.searchinput} placeholder="Buscar Pokémon" type="search" onChange={onChangeHandle} />
-                <button className={style.pokeButton} onClick={onButtonClickHandle}>
-                    <img className={style.pokeButtonImg} src="src\assets\Pokebola-pokeball-png-0.png" alt="pokeButton" />
+                <input
+                    className={style.searchinput}
+                    placeholder="Buscar Pokémon"
+                    type="search"
+                    value={search}
+                    onChange={onChangeHandle}
+                />
+
+                <button
+                    className={style.pokeButton}
+                    onClick={onButtonClickHandle}
+                >
+                    <img
+                        className={style.pokeButtonImg}
+                        src="src/assets/Pokebola-pokeball-png-0.png"
+                        alt="pokeButton"
+                    />
                 </button>
             </div>
-           
         </div>
-
-    )
-}
+    );
+};
